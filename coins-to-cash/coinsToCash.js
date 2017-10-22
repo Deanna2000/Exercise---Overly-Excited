700255a050acbcfdeb7ffd5a5e80551318554096
Deanna Vickers <deanna2000@gmail.com>
2017-10-22 14:29:03 -0500

    setting up the pb object

---
+++ coins-to-cash/coinsToCash.js
@@ -0,0 +1,13 @@
+//create the object for piggybank and define keys for the coins
+const piggyBank = {
+    "quarters": 25,
+    "nickels": 14,
+    "dimes": 74,
+    "pennies": 342
+}
+
+let dollarAmount = 0
+
+let dollarAmount = piggyBank.quarters/4 + piggyBank.nickels/20 + piggyBank.dimes/10 + piggyBank.pennies/100
+
+console.log (dollarAmount)
