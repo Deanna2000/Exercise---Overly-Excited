2f33b743f4359f428dcfd51373900fd487009d57
Deanna Vickers <deanna2000@gmail.com>
2017-10-22 18:14:47 -0500

    fixed the calculations

---
+++ cash-to-coins/cashToCoins.js
@@ -0,0 +1,30 @@
+// set the dollar amount we are calculating
+let dollarAmount = 2.98
+const piggyBank = {
+    quarters: .25,
+    dimes: .10,
+    nickels: .05,
+    pennies: .01,
+}
+
+let keyAmount = 0;
+
+//set up something to deplete dollarAmount and identify the number of coins (quarters, etc.)
+
+keyAmount = {} //declare keyAmount
+
+for (var key in piggyBank) {
+    if (piggyBank.hasOwnProperty(key)) {
+        let keyAmount = Math.floor(dollarAmount/piggyBank[key])
+        if (keyAmount>0) {
+            console.log("Number of", (key), ": ",keyAmount)
+        }
+        else {
+            console.log("Number of", (key),": ", 0)
+        }
+            dollarAmount -= (keyAmount*(piggyBank[key]))
+            console.log("Remaining", dollarAmount)
+        }
+    }
+
+
