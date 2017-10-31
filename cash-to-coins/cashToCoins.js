24801ed3951760ccb202441d7aa7a793b2ebfe69
Deanna Vickers <deanna2000@gmail.com>
2017-10-31 09:52:33 -0500

    fixed rounding issue and replaced array values

--- cash-to-coins/cashToCoins.js
+++ cash-to-coins/cashToCoins.js
@@ -1,10 +1,10 @@
 //Set the dollar amount we are calculating
-let dollarAmount = 2.98
+let dollarAmount = 2.98*100
 const piggyBank = {
-    quarters: .25,
-    dimes: .10,
-    nickels: .05,
-    pennies: .01,
+    quarters: 25,
+    dimes: 10,
+    nickels: 05,
+    pennies: 01,
 }
 
 let keyAmount = 0;
@@ -15,17 +15,18 @@ keyAmount = {} //declare keyAmount
 
 for (var key in piggyBank) {
     if (piggyBank.hasOwnProperty(key)) {
-        let keyAmount = Math.floor(dollarAmount / piggyBank[key])
+            let keyAmount = Math.floor(dollarAmount / piggyBank[key])
        
-        if (keyAmount > 0) {
+            if (keyAmount > 0) {
             console.log("Number of", (key), ": ", keyAmount)
-            dollarAmount -= (keyAmount * (piggyBank[key]))//rounding issue here
-            console.log("Remaining", dollarAmount)
+            dollarAmount -= (keyAmount * (piggyBank[key]))  //rounding issue here
+            piggyBank[key] = keyAmount
         }
-        else {
+            else {
             console.log("Number of", (key), ": ", 0)
+            piggyBank[key] = keyAmount
         }
     }
 }
-
+console.log(piggyBank)
 
---
+++ colored-reindeer/reindeer-factory.js
@@ -0,0 +1,26 @@
+const coloredReindeerBuilder = function () {
+    const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    const colors = ["Blue", "Red", "Orange", "Purple", "Goldenrod", 
+    "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
+    
+    let reindeer = 0
+
+    const reindeerCounter
+
+    // Write a for loop that looks at each reindeer
+        const getNextColor = function () {
+            const colors = ["Red", "Yellow", "Green", "Goldenrod"]
+            let counter = 0
+        
+            const currentColor = colors[counter] // Red
+            counter += 1  // Increment the counter
+            return currentColor
+
+    }
+        // Invoke factory function to create reindeer object
+
+        // Put new reindeer object in coloredReindeer array
+
+
+    // Return coloredReindeer array
+}
